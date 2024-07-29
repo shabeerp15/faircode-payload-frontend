b8d15f302ba7103c36107ebc41d5f0f14c229e57
shabeerp15 <shabeerp15@gmail.com>
2024-07-29 12:11:55 +0530

    Comment footer section

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { CMSLink } from "../CMSLink";
 
 const Footer = async () => {
 	const response = await axios.get(`${process.env.NEXT_PUBLIC_PAYLOAD_URL}/api/globals/footer`);
-	const data = response.data;
+	const data = response?.data;
 
 	return (
 		<footer className="bg-white pt-12 px-4 sm:px-6 lg:px-8">
@@ -12,16 +12,17 @@ const Footer = async () => {
 				<div className="grid grid-cols-1 md:grid-cols-4 gap-8">
 					{/* Logo */}
 					<div className="col-span-1">
-						<img src={process.env.NEXT_PUBLIC_PAYLOAD_URL + data.logo.url} alt="Faircode" className="h-8 w-auto" />
+						<img src={process.env.NEXT_PUBLIC_PAYLOAD_URL + data?.logo?.url} alt="Faircode" className="h-8 w-auto" />
 					</div>
 
 					{/* Address */}
 					<div className="col-span-1">
-						{data.address[0].children.map((item: any, index: any) => (
-							<p key={index} className={`text-sm text-gray-600 ${item.bold && "font-bold"} ${item.italic && "italic"} ${item.underline && "underline"} `}>
-								{item.text}
-							</p>
-						))}
+						{data &&
+							data?.address[0]?.children?.map((item: any, index: any) => (
+								<p key={index} className={`text-sm text-gray-600 ${item.bold && "font-bold"} ${item.italic && "italic"} ${item.underline && "underline"} `}>
+									{item.text}
+								</p>
+							))}
 					</div>
 
 					{/* Work Inquiries */}
@@ -39,23 +40,24 @@ const Footer = async () => {
 						<a href="mailto:hr@faircode.com" className="text-sm text-gray-900 font-semibold hover:underline">
 							hr@faircode.com
 						</a> */}
-						{data.contact?.map((item: any, index: any) => (
-							<div className="mb-8 border-b pb-2 ">
-								<h3 className="text-sm font-semibold text-gray-900 uppercase mt-4 mb-2">{item.title}</h3>
-								<p className="text-sm text-gray-600">{item.highlightText}</p>
-								<a href={`mailto:${item.email}`} className="text-sm text-gray-900 font-semibold hover:underline">
-									{item.email}
-								</a>
-							</div>
-						))}
+						{data &&
+							data?.contact?.map((item: any, index: any) => (
+								<div className="mb-8 border-b pb-2 ">
+									<h3 className="text-sm font-semibold text-gray-900 uppercase mt-4 mb-2">{item.title}</h3>
+									<p className="text-sm text-gray-600">{item.highlightText}</p>
+									<a href={`mailto:${item.email}`} className="text-sm text-gray-900 font-semibold hover:underline">
+										{item.email}
+									</a>
+								</div>
+							))}
 					</div>
 
 					{/* Newsletter */}
 					<div className="col-span-1">
-						<h3 className="text-sm font-semibold text-gray-900 mb-2">{data.newsletter.title}</h3>
+						<h3 className="text-sm font-semibold text-gray-900 mb-2">{data?.newsletter?.title}</h3>
 						<form className="mt-2">
 							<div className="flex items-center border border-gray-300 p-2">
-								<input type="email" placeholder={data.newsletter.placeholderText} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" />
+								<input type="email" placeholder={data?.newsletter?.placeholderText} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" />
 								<button type="submit" className="flex-shrink-0 text-gray-500 hover:text-gray-700">
 									<svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
 										<path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
@@ -63,7 +65,7 @@ const Footer = async () => {
 								</button>
 							</div>
 						</form>
-						<p className="text-xs text-gray-500 mt-3">{data.newsletter.highlightText}</p>
+						<p className="text-xs text-gray-500 mt-3">{data?.newsletter?.highlightText}</p>
 					</div>
 				</div>
 
@@ -87,11 +89,12 @@ const Footer = async () => {
 
 				{/* Bottom Bar */}
 				<div className=" border-t border-gray-200 py-4 flex flex-wrap justify-between items-center ">
-					<p className="text-sm text-gray-500 ">{data.bottomBar[0].copyright}</p>
+					<p className="text-sm text-gray-500 ">{data?.bottomBar[0]?.copyright}</p>
 					<nav className="flex space-x-4 text-sm ">
-						{data?.bottomBar[0]?.navItems?.map((item: any, index: any) => (
-							<CMSLink type={item?.link?.type} url={item?.link?.url} label={item?.link?.label} className="text-gray-500 hover:text-gray-600" key={index} />
-						))}
+						{data &&
+							data?.bottomBar[0]?.navItems?.map((item: any, index: any) => (
+								<CMSLink type={item?.link?.type} url={item?.link?.url} label={item?.link?.label} className="text-gray-500 hover:text-gray-600" key={index} />
+							))}
 					</nav>
 				</div>
 			</div>
