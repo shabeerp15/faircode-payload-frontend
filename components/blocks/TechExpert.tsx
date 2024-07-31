538bd5dcf2afd725c2154e6a0c3fc6e54b6bf0c9
aswinjithkukku <aswinjitht5@gmail.com>
2024-07-31 13:18:50 +0530

    tech expert update

--- components/blocks/TechExpert.tsx
+++ components/blocks/TechExpert.tsx
@@ -12,13 +12,26 @@ interface TechExpertBlockProps {
 	imageSrc?: string;
 }
 
-const TechExpertBlock = ({ title, subtitle, description, ctaText, ctaLink, imageSrc }: TechExpertBlockProps) => {
+const TechExpertBlock = ({
+	title,
+	subtitle,
+	description,
+	ctaText,
+	ctaLink,
+	imageSrc,
+}: TechExpertBlockProps) => {
 	return (
-		<div className="flex flex-col md:flex-row bg-black text-white">
-			<div className="w-full md:w-1/2 bg-black text-white p-5 md:p-8 lg:p-24 flex flex-col justify-center ">
-				<h2 className="text-xl md:text-3xl font-semibold mb-2">{title}</h2>
-				<h1 className="text-3xl md:text-6xl lg:text-7xl font-bold mb-4">{subtitle}</h1>
-				<p className="text-md mb-8 text-[#C1C1C1] lg:w-2/3 font-thin">{description}</p>
+		<div className="flex flex-col md:flex-row bg-black text-white h-auto">
+			<div className="w-full min-h-[600px] md:w-1/2 bg-black text-white p-5 md:p-8 lg:p-24 flex flex-col justify-center ">
+				<h2 className="text-xl md:text-3xl font-semibold mb-2">
+					{title}
+				</h2>
+				<h1 className="text-3xl md:text-6xl lg:text-7xl font-bold mb-4">
+					{subtitle}
+				</h1>
+				<p className="text-md mb-8 text-[#C1C1C1] lg:w-2/3 font-thin">
+					{description}
+				</p>
 				<div className="flex items-center gap-3">
 					<button className="bg-[#00F5A3] text-black font-semibold p-3 pl-1 rounded-full inline-flex items-center transition duration-300 hover:bg-green-500 w-fit">
 						<ArrowRight className="ml-2" size={20} />
@@ -26,10 +39,23 @@ const TechExpertBlock = ({ title, subtitle, description, ctaText, ctaLink, image
 					<Link href={ctaLink}>{ctaText}</Link>
 				</div>
 			</div>
-			<div className="w-full md:w-1/2 bg-green-400 flex items-center justify-center">
-				<div className="relative">
-					{/* <Image src={`${process.env.NEXT_PUBLIC_PAYLOAD_URL}/media/logo_faircode.svg`} alt="Smartphone app screenshot" layout="fill" objectFit="contain" /> */}
-					{/* <img src={`${process.env.NEXT_PUBLIC_PAYLOAD_URL}${imageSrc}`} alt="Smartphone app screenshot" className=" object-cover w-full h-full" /> */}
+			<div className="w-full min-h-[600px] md:w-1/2 bg-[#00F5A3] flex items-center justify-center relative">
+				<div className="absolute right-0 bottom-0">
+					<img
+						src={`${process.env.NEXT_PUBLIC_PAYLOAD_URL}${imageSrc}`}
+						alt={"img"}
+						className=" w-auto  max-h-[500px]"
+					/>
+				</div>
+				<div className="absolute bg-white h-48 w-48 bottom-0 left-0 flex flex-col justify-center items-center p-5 gap-2">
+					{/* <img
+							src={imgData?.imageFields?.secondaryImage?.url}
+							alt={imgData?.imageFields?.secondaryImage?.alt}
+							className="h-10 object-contain"
+						/> */}
+					<p className="font-grotesk font-black text-xl text-black">
+						This is a custom text
+					</p>
 				</div>
 			</div>
 		</div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["localhost"],
-    }
+	images: {
+		domains: [
+			"localhost",
+			"payload-test-production-a817.up.railway.app",
+		],
+	},
 };
 
 export default nextConfig;
